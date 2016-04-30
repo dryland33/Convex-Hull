63e75a0e5a4bafbff10946b330e1cd9761a9dccd
dryland33 <dryland28@gmail.com>
2016-04-30 16:32:05 -0400

    Update jmarch.js

--- Jarvis-March/jmarch.js
+++ Jarvis-March/jmarch.js
@@ -1,4 +1,3 @@
-
 function getRandomPoints(numPoints) {
 
     var points = new Array();
@@ -13,20 +12,45 @@ function getRandomPoints(numPoints) {
         points.push([x, y]);
     }
 
-    return points
+return points
 }
 
-var points;
+var my_points;
 
 function jmPlotPoints() {
 
     contex = document.getElementById('jm_demo').getContext('2d');
     contex.clearRect(0, 0, 650, 650);
     contex.fillStyle = 'rgb(0,0,0)';
-    points = getRandomPoints(500);
+    my_points = getRandomPoints(5);
+
+    for (var index in my_points) {
+        var point = my_points[index];
+        contex.fillRect(point[0], point[1], 2, 2);
+    }
+}
+
+function findLeftMost(points) {
+
+    var min_x;
+    var curr_min;
+    var left_index;
 
     for (var index in points) {
         var point = points[index];
-        contex.fillRect(point[0], point[1], 2, 2);
+        if (point[0] < min_x || !min_x) {
+            curr_min = point;
+            min_x = point[0];
+            left_index = index;
+        }
     }
+
+    var leftMost = points[left_index];
+
+return leftMost;
+}
+
+function jmPlotConvexHull() {
+    var left_point = findLeftMost(my_points);
+    window.alert("Left point's x value is: " + left_point[0]);
 }
